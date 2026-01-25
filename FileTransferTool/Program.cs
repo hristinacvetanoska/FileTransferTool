181eb69f38047acd46eb08449625a531f67908c0
PC <PC@DESKTOP-RHGSQDI>
2026-01-25 16:15:16 +0100

    Add project files.

---
+++ FileTransferTool/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var sourceFilePath = String.Empty;
+        var destination = String.Empty;
+        while (true) {
+            Console.Write("Enter source file path (e.g. c:\\source\\my_large_file.bin)");
+            sourceFilePath = Console.ReadLine().Trim();
+            if (File.Exists(sourceFilePath))
+            {
+                break;
+            }
+            Console.WriteLine("Error: The File doesn't exist, please try again.");
+        }
+
+        while (true) {
+
+            Console.Write("Enter destination path (e.g. d:\\destination\\)");
+            destination = Console.ReadLine().Trim();
+            if (Directory.Exists(destination))
+            {
+                break;
+            }
+            Console.WriteLine($"Directory {destination} does not exist!");
+        }
+        var destionationFilePath = Path.Combine(destination, Path.GetFileName(sourceFilePath));
+        Console.WriteLine($"Full destination file path is: {destionationFilePath}");
+        Console.ReadLine();
+    }
+}
